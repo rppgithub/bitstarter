fc8e02b82e559001e4d6ac58571ffb68eb8ac811
R. Prasad <rpprasad@gmail.com>
2015-06-11 12:26:05 -0700

    New revision

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var express = require('express')
+var fs = require('fs');
+var HTMLFILE_DEFAULT="index.html";
+
+var express = require('express');
 var app = express();
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  fs.readFileSync('index.html',function(err,data) {
+	if (err) throw err;
+        buf = new Buffer(30);
+  	response.send(buf.toString(data));
+  });
 })
 
 app.listen(app.get('port'), function() {
