95c49d1d83febb6790b69bf62f1e9893dc4a4112
R. Prasad <rpprasad@gmail.com>
2015-06-11 12:49:13 -0700

    New revision

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html',function(err,data) {
-	if (err) throw err;
-        buf = new Buffer(30);
-  	response.send(buf.toString(data));
-  });
+        var buf = fs.readFileSync('index.html') ;
+  	response.send(buf);
 })
 
 app.listen(app.get('port'), function() {
